66e9d000bbe15645f88cd0b6df8665615cae0e86
Juan Ferrari <juannferrari@gmail.com>
2025-04-11 20:53:58 +0000

    Update sync reports and discover.py

--- tap_doubleclick_campaign_manager/discover.py
+++ tap_doubleclick_campaign_manager/discover.py
@@ -1,7 +1,5 @@
 import re
-
 from singer.catalog import Catalog, CatalogEntry, Schema
-
 from tap_doubleclick_campaign_manager.schema import (
     SINGER_REPORT_FIELD,
     get_fields,
@@ -9,9 +7,11 @@ from tap_doubleclick_campaign_manager.schema import (
     get_field_type_lookup
 )
 
+
 def sanitize_name(report_name):
     report_name = re.sub(r'[\s\-\/]', '_', report_name.lower())
     return re.sub(r'[^a-z0-9_]', '', report_name)
+
 
 def discover_streams(service, config):
     profile_id = config.get('profile_id')
@@ -21,31 +21,37 @@ def discover_streams(service, config):
         .reports()
         .list(profileId=profile_id)
         .execute()
-        .get('items')
+        .get('items', [])
     )
 
     reports = sorted(reports, key=lambda x: x['id'])
     report_configs = {}
     for report in reports:
-        stream_name = sanitize_name(report['name'])
-        tap_stream_id = '{}_{}'.format(stream_name, report['id'])
-        report_configs[(stream_name, tap_stream_id)] = report
+        stream_base_name = sanitize_name(report['name'])
+        tap_stream_id = f"{stream_base_name}_{report['id']}"  # unique identifier
+        report_configs[tap_stream_id] = {
+            "stream_name": stream_base_name,
+            "tap_stream_id": tap_stream_id,
+            "report": report
+        }
 
     field_type_lookup = get_field_type_lookup()
     catalog = Catalog([])
 
-    for (stream_name, tap_stream_id), report in report_configs.items():
+    for tap_stream_id, cfg in report_configs.items():
+        stream_name = cfg["stream_name"]
+        report = cfg["report"]
+
         fieldmap = get_fields(field_type_lookup, report)
-        schema_dict = get_schema(stream_name, fieldmap)
+        schema_dict = get_schema(tap_stream_id, fieldmap)
         schema = Schema.from_dict(schema_dict)
 
-        metadata = []
-        metadata.append({
+        metadata = [{
             'metadata': {
                 'tap-doubleclick-campaign-manager.report-id': report['id']
             },
             'breadcrumb': []
-        })
+        }]
 
         for prop in schema_dict['properties'].keys():
             metadata.append({
@@ -56,8 +62,8 @@ def discover_streams(service, config):
             })
 
         catalog.streams.append(CatalogEntry(
-            stream=stream_name,
-            stream_alias=stream_name,
+            stream=tap_stream_id,
+            stream_alias=tap_stream_id,
             tap_stream_id=tap_stream_id,
             key_properties=[],
             schema=schema,
