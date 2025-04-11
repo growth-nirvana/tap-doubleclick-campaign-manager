fe15a9a3dfcd407c02426bb849658270da7ee2ed
Juan Ferrari <juannferrari@gmail.com>
2025-04-11 18:27:29 +0000

    Fix sync reports

--- tap_doubleclick_campaign_manager/sync_reports.py
+++ tap_doubleclick_campaign_manager/sync_reports.py
@@ -18,10 +18,10 @@ from tap_doubleclick_campaign_manager.schema import (
 
 LOGGER = singer.get_logger()
 
-MIN_RETRY_INTERVAL = 2 # 10 seconds
-MAX_RETRY_INTERVAL = 300 # 5 minutes
-MAX_RETRY_ELAPSED_TIME = 3600 # 1 hour
-CHUNK_SIZE = 16 * 1024 * 1024 # 16 MB
+MIN_RETRY_INTERVAL = 2  # 10 seconds
+MAX_RETRY_INTERVAL = 300  # 5 minutes
+MAX_RETRY_ELAPSED_TIME = 3600  # 1 hour
+CHUNK_SIZE = 16 * 1024 * 1024  # 16 MB
 
 class StreamFunc(object):
     def __init__(self, func):
@@ -60,31 +60,44 @@ def parse_line(line):
 def transform_field(dfa_type, value):
     if value == '':
         return None
-    if dfa_type == 'double':
-        return float(value)
-    if dfa_type == 'long':
-        try:
-            return int(value)
-        except:
-            return None
-    if dfa_type == 'boolean':
-        value = value.lower().strip()
-        return (
-            value == 'true' or
-            value == 't' or
-            value == 'yes' or
-            value == 'y'
-        )
 
     if isinstance(dfa_type, list):
         for t in dfa_type:
             try:
                 return transform_field(t, value)
-            except ValueError as error:
-                LOGGER.warning(f'Failed parse field as {t}')
+            except Exception:
                 continue
+        return value
+
+    try:
+        if dfa_type == 'double':
+            return float(value)
+        elif dfa_type == 'long':
+            return int(value)
+        elif dfa_type == 'boolean':
+            value = value.lower().strip()
+            return value in ('true', 't', 'yes', 'y')
+        elif dfa_type == 'string':
+            return value
+    except Exception:
+        return value
 
     return value
+
+def normalize_types(obj, fieldmap):
+    for field in fieldmap:
+        field_name = field['name']
+        expected_type = field['type']
+        val = obj.get(field_name)
+
+        if isinstance(val, str) and val.isdigit():
+            if isinstance(expected_type, list):
+                if 'integer' in expected_type:
+                    obj[field_name] = int(val)
+            elif expected_type == 'integer':
+                obj[field_name] = int(val)
+
+    return obj
 
 def process_file(service, fieldmap, report_config, file_id, report_time):
     report_id = report_config['report_id']
@@ -112,27 +125,27 @@ def process_file(service, fieldmap, report_config, file_id, report_time):
 
         if line_state['past_headers']:
             row = parse_line(line)
-            # skip report grant total line
             if row[0] == 'Grand Total:':
                 return
 
             obj = {}
             for i in range(len(fieldmap)):
                 field = fieldmap[i]
-                obj[field['name']] = transform_field(field['type'], row[i])
+                val = transform_field(field['type'], row[i])
+                obj[field['name']] = val
 
             obj[SINGER_REPORT_FIELD] = report_time
             obj[REPORT_ID_FIELD] = report_id_int
+
+            obj = normalize_types(obj, fieldmap)
 
             singer.write_record(stream_name, obj, stream_alias=stream_alias)
             line_state['count'] += 1
 
     stream = StreamFunc(line_transform)
-    downloader = http.MediaIoBaseDownload(stream,
-                                          request,
-                                          chunksize=CHUNK_SIZE)
+    downloader = http.MediaIoBaseDownload(stream, request, chunksize=CHUNK_SIZE)
     download_finished = False
-    while download_finished is False:
+    while not download_finished:
         _, download_finished = downloader.next_chunk()
 
     with singer.metrics.record_counter(stream_name) as counter:
@@ -145,83 +158,47 @@ def sync_report(service, field_type_lookup, profile_id, report_config):
 
     LOGGER.info("%s: Starting sync", stream_name)
 
-    report = (
-        service
-        .reports()
-        .get(profileId=profile_id,
-             reportId=report_id)
-        .execute()
-    )
-
+    report = service.reports().get(profileId=profile_id, reportId=report_id).execute()
     fieldmap = get_fields(field_type_lookup, report)
     schema = get_schema(stream_name, fieldmap)
     singer.write_schema(stream_name, schema, [], stream_alias=stream_alias)
 
     with singer.metrics.job_timer('run_report'):
         report_time = datetime.utcnow().isoformat() + 'Z'
-        report_file = (
-            service
-            .reports()
-            .run(profileId=profile_id,
-                 reportId=report_id)
-            .execute()
-        )
-
+        report_file = service.reports().run(profileId=profile_id, reportId=report_id).execute()
         report_file_id = report_file['id']
 
         sleep = 0
         start_time = time.time()
         while True:
-            report_file = (
-                service
-                .files()
-                .get(reportId=report_id,
-                     fileId=report_file_id)
-                .execute()
-            )
-
+            report_file = service.files().get(reportId=report_id, fileId=report_file_id).execute()
             status = report_file['status']
 
             if status == 'QUEUED':
                 sleep = next_sleep_interval(sleep)
-                LOGGER.info('{}: report_id {} / file_id {} - File status is {}, sleeping for {} seconds'.format(
-                    stream_name,
-                    report_id,
-                    report_file_id,
-                    status,
-                    sleep))
+                LOGGER.info('%s: report_id %s / file_id %s - File status is %s, sleeping for %s seconds',
+                            stream_name, report_id, report_file_id, status, sleep)
                 time.sleep(sleep)
 
             elif status == 'REPORT_AVAILABLE':
-                LOGGER.info('Report file {} had status of {}; beginning file processing.'.format(
-                    report_file_id,
-                    status))
+                LOGGER.info('Report file %s had status of %s; beginning file processing.', report_file_id, status)
                 process_file(service, fieldmap, report_config, report_file_id, report_time)
                 break
 
             elif status != 'PROCESSING':
-                message = '{}: report_id {} / file_id {} - File status is {}, processing failed'.format(
-                        stream_name,
-                        report_id,
-                        report_file_id,
-                        status)
+                message = ('%s: report_id %s / file_id %s - File status is %s, processing failed'
+                           % (stream_name, report_id, report_file_id, status))
                 LOGGER.error(message)
                 raise Exception(message)
 
             elif time.time() - start_time > MAX_RETRY_ELAPSED_TIME:
-                message = '{}: report_id {} / file_id {} - File processing deadline exceeded ({} secs)'.format(
-                        stream_name,
-                        report_id,
-                        report_file_id,
-                        status,
-                        MAX_RETRY_ELAPSED_TIME)
+                message = ('%s: report_id %s / file_id %s - File processing deadline exceeded (%s secs)'
+                           % (stream_name, report_id, report_file_id, MAX_RETRY_ELAPSED_TIME))
                 LOGGER.error(message)
                 raise Exception(message)
 
-
 def sync_reports(service, config, catalog, state):
     profile_id = config.get('profile_id')
-
     reports = []
     for stream in catalog.streams:
         mdata = singer.metadata.to_map(stream.metadata)
@@ -232,9 +209,9 @@ def sync_reports(service, config, catalog, state):
                 'stream_name': stream.tap_stream_id,
                 'stream_alias': stream.stream_alias
             })
+
     reports = sorted(reports, key=lambda x: x['report_id'])
 
-    # if report selection changes, we want to start over
     if state.get('reports') != reports:
         state['current_report'] = None
         state['reports'] = reports
@@ -246,19 +223,14 @@ def sync_reports(service, config, catalog, state):
     for report_config in reports:
         report_id = report_config['report_id']
 
-        if current_report is not None and \
-           not past_current_report and \
-           current_report != report_id:
+        if current_report is not None and not past_current_report and current_report != report_id:
             continue
 
         past_current_report = True
         state['current_report'] = report_id
         singer.write_state(state)
 
-        sync_report(service,
-                    field_type_lookup,
-                    profile_id,
-                    report_config)
+        sync_report(service, field_type_lookup, profile_id, report_config)
 
     state['reports'] = None
     state['current_report'] = None
